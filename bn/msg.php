f5ba3d3914004c024093635c0f9881e33dac9631
soovik-git <126764494+soovik-git@users.noreply.github.com>
2023-07-03 08:26:05 +0530

    Add files via upload

---
+++ bn/msg.php
@@ -0,0 +1,112 @@
+<?php
+    return [
+        
+            "Home" => "বাড়ি",
+"Services" => "সেবা",
+"Contact" => "যোগাযোগ",
+"Gallery" => "গ্যালারী",
+"Login" => "প্রবেশ করুন",
+"Get the best" => "সেরা পেতে",
+"Medical Consultation, Medicine Order, Medical Visit" => "চিকিত্সা পরামর্শ, মেডিসিন অর্ডার, মেডিকেল ভিজিট",
+"and many more..." => "এবং আরো অনেক...",
+"\"Nature Image #2Nature Image #3" => "\"Nature Image #2Nature Image #3",
+"Serving Patients" => "সেবা রোগীদের",
+"in India, Nepal and Bangladesh" => "ভারতে, নেপাল এবং বাংলাদেশে",
+"Trusted VISA and Medical Visit Services" => "বিশ্বস্ত ভিসা এবং মেডিকেল ভিজিট পরিষেবা",
+"Book Now" => "এখনই বুক করুন",
+"Medicines Order and" => "ওষুধের আদেশ এবং",
+"Fastest Medicine delivery" => "দ্রুততম ওষুধ সরবরাহ",
+"and many more..." => "এবং আরো অনেক...",
+"Order Now" => "এখনি আদেশ কর",
+"Patient served" => "রোগী পরিবেশন করা",
+"Doctors tied" => "চিকিত্সকরা বাঁধা",
+"Hospitals tied" => "হাসপাতাল বেঁধে",
+        
+          
+"Medical visits for patients" => "রোগীদের জন্য মেডিকেল ভিজিট",
+"Our Services" => "আমাদের সেবাসমূহ",
+"Medical Consultation" => "ডাক্তারী পরামর্শ",
+"Medicine order" => "মেডিসিন অর্ডার",
+"lab test" => "ল্যাব পরীক্ষা",
+"Visa & Passports for medical visits:" => "মেডিকেল ভিজিটের জন্য ভিসা এবং পাসপোর্ট:",
+"Travel Support for medical purposes:" => "চিকিত্সা উদ্দেশ্যে ভ্রমণ সমর্থন:",
+"Meet Our Experts" => "আমাদের বিশেষজ্ঞদের সাথে দেখা করুন",
+"In today's society common people are not aware about their health-related information. Even though the current government has given them various health related facilities, the common people cannot get all those facilities properly, because they do not know the details about how and where to get those facilities. InstaDoctor PVT. LTD." => "আজকের সমাজে সাধারণ লোকেরা তাদের স্বাস্থ্য সম্পর্কিত তথ্য সম্পর্কে অবগত নয়। যদিও বর্তমান সরকার তাদের বিভিন্ন স্বাস্থ্য সম্পর্কিত সুবিধা দিয়েছে, সাধারণ লোকেরা এই সমস্ত সুবিধাগুলি সঠিকভাবে পেতে পারে না, কারণ তারা কীভাবে এবং কোথায় এই সুবিধাগুলি পাবেন সে সম্পর্কে বিশদগুলি জানেন না। ইনস্টাডোক্টর প্রাইভেট। লিমিটেড",
+"Learn More" => "আরও জানুন",
+"A day in our lives" => "আমাদের জীবনের একটি দিন",
+"Our Special Events" => "আমাদের বিশেষ ইভেন্টসমূহ",
+"Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics" => "ক্লাসিক্যাল পদার্থবিদ্যার দুটি প্রধান মহাদেশের মধ্যে সংঘর্ষ নিরসনে সমস্যাগুলি চেষ্টা করছে: নিউটনীয় যান্ত্রিকতা সমস্যাগুলি চেষ্টা করছে",
+"Check out more Events" => "আরো ইভেন্টগুলি দেখুন",
+"We at work" => "আমরা কাজে",
+"Our Videos" => "আমাদের ভিডিওগুলি",
+"Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics" => "ক্লাসিক্যাল পদার্থবিদ্যার দুটি প্রধান মহাদেশের মধ্যে সংঘর্ষ নিরসনে সমস্যাগুলি চেষ্টা করছে: নিউটনীয় যান্ত্রিকতা সমস্যাগুলি চেষ্টা করছে",
+"Testimonials" => "প্রশংসাপত্রগুলি",
+
+"Our Happy Customers" => "আমাদের খুশি গ্রাহকরা",
+"Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics" => "ধ্রুপদী পদার্থবিজ্ঞানের দুটি প্রধান ক্ষেত্রের মধ্যে দ্বন্দ্ব সমাধানের চেষ্টা করার সমস্যাগুলি: নিউটনীয় যান্ত্রিক",
+"Any Queries" => "কোন প্রশ্ন",
+"Contact Us" => "যোগাযোগ করুন",
+"Do you have any queries, suggestions, or requests? InstaDoctor is a chain of patient care facilities. To contact us, please fill out the form below." => "আপনার কি কোনও প্রশ্ন, পরামর্শ বা অনুরোধ আছে? ইনস্টাডোক্টর হ'ল রোগীর যত্নের সুবিধার একটি শৃঙ্খলা। আমাদের সাথে যোগাযোগ করতে, দয়া করে নীচের ফর্মটি পূরণ করুন।",
+"Contact Information" => "যোগাযোগের তথ্য",
+"Your Name" => "তোমার নাম",
+"Your Email" => "তোমার ইমেইল",
+"Your Email" => "তোমার ইমেইল",
+"Head Office Address:" => "প্রধান অফিসের ঠিকানা:",
+"Hospital Road, Janaushadhi Building, 1st Floor, P.S : Madarihat, P.O : Madarihat, Dist: -Alipurduar, PIN: 735220" => "হাসপাতাল রোড, জনশাধি বিল্ডিং, প্রথম তল, পি.এস. : মদারিহাট, পোস্ট অফিস: মদারিহাট, জেলা: -আলিপুরদুয়ার, পিন: ৭৩৫২২০",
+"Phone Number:" => "ফোন নম্বর:",
+"Whatsapp:" => "হোয়াটসঅ্যাপ:",
+"Email:" => "ইমেল:",
+"send" => "প্রেরণ",
+"Menu" => "তালিকা",
+
+"Doctors with us" => "আমাদের সাথে চিকিত্সকরা",
+"Hospitals with us" => "আমাদের সাথে হাসপাতাল",
+"Terms and Conditions" => "শর্তাবলী",
+"Return and Refund Policy" => "প্রত্যাবর্তন এবং ফেরত নীতি",
+"Shipping Policy" => "চালান নীতি",
+"Privacy policy" => "গোপনীয়তা নীতি",
+"Download our Android App" => "আমাদের অ্যান্ড্রয়েড অ্যাপটি ডাউনলোড করুন",
+"Get In Touch" => "যোগাযোগ পেতে",
+"Kolkata Office" => "Prince Anwar Shah road, Tollygaunge, Kolkata",
+"Nepal Office" => "Near Purano Bus Park, Bagbazar, Katmandu, 44600, Nepal",
+
+
+
+        'Contact' =>"যোগাযোগের নম্বর",
+
+        'whatsapp'=>"হোয়াটসঅ্যাপ",
+
+        'Email'=>"ইমেইল",
+
+        'bannerTitle1'=>"সেরা চিকিৎসা পরামর্শ পান মেডিসিন অর্ডার এবং আপনার চিকিৎসা পরিদর্শনে সাহায্য",
+
+        'About' => "আমাদের সম্পর্কে",
+
+        'AboutDescription'=>"আমাদের পরিষেবাগুলি রোগীদের সাহায্য করে যেখানে দূরত্ব একটি গুরুত্বপূর্ণ কারণ। আমরা ব্যবধান পূরণ করি এবং তাদের সর্বোত্তম চিকিৎসা সুবিধার সাথে যোগাযোগ করি। তাই আমাদের পরিষেবাগুলি দূরবর্তী স্থান থেকে আগত রোগীদের উন্নত রোগ নির্ণয়ের জন্য তথ্য প্রদান এবং অ্যালোপ্যাথিক, হোমিওপ্যাথিক এবং আয়ুর্বেদিক চিকিৎসার জন্য সহায়তা প্রদানের মতো সুবিধা দিয়ে সাহায্য করতে পারে। আমাদের নির্দিষ্ট সুবিধাগুলি রোগীদের আমাদের বিশেষজ্ঞ ডাক্তারদের সাথে অনলাইন এবং অফলাইন উভয় পরামর্শ গ্রহণ করতে, ওষুধ অর্ডার করতে, ডায়াগনস্টিক পরীক্ষার অনুরোধ করতে এবং ভিসা বা পাসপোর্ট-সম্পর্কিত বিষয়ে সহায়তা পেতে সক্ষম করবে, ভারতের অভ্যন্তরে সমস্ত টিকিটিং, বিমানবন্দর থেকে পিক আপ এবং ড্রপ পরিষেবা, ট্রেন স্টেশন এবং বাস স্টপ থাকার ব্যবস্থা, মেডিকেল গাইড এবং আরও অনেক কিছু।",
+        'Services'=>"আমাদের সেবাসমূহ",
+
+        'Medical-Consultation'=>"ডাক্তারী পরামর্শ",
+
+        'MedicalDescription'=>"আপনি কি বিশ্বস্ত ডাক্তারদের সাথে পরামর্শ করতে এবং সাশ্রয়ী মূল্যে চিকিৎসা সুবিধা পেতে চাইছেন? আমাদের সাথে নিবন্ধিত সেরা ডাক্তাররা আছেন যারা শুধুমাত্র অনলাইনে পরামর্শ প্রদান করেন না কিন্তু অফলাইনেও যেতে পারেন। আরও বিস্তারিত জানার জন্য নিচের নম্বরে আমাদের কল করুন।",
+        
+        'Medical-order'=>"মেডিসিন অর্ডার",
+
+        'medicalOrder-description'=>"আপনি কি কিছু ওষুধ অর্ডার করতে চান এবং ভারতের বাইরে থেকে যুক্তিসঙ্গত মূল্যে অবিলম্বে সরবরাহ করতে চান? আমাদের কাছে ডেলিভারির একটি চমৎকার চেইন রয়েছে যা নিশ্চিত করে যে আপনার ওষুধ সময়মতো আপনার কাছে পৌঁছায়। ঔষধ অর্ডার করতে নিচের নাম্বারে কল করুন।",
+
+        'labTest'=>"ল্যাব পরীক্ষা",
+
+        'labTest-description'=>"আমাদের InstaDoctor নিবন্ধিত ল্যাবগুলির সাথে আপনার ল্যাব পরীক্ষা বুক করুন। আপনার সমস্ত ল্যাব পরীক্ষাগুলি যুক্তিসঙ্গত হারে সম্পন্ন করুন এবং ঝামেলামুক্ত নমুনা সংগ্রহ এবং রিপোর্ট ডেলিভারি পান। একটি পরীক্ষা বুক করার জন্য নীচের নম্বরে আমাদের কল করুন|",
+
+        'Visa'=>"চিকিৎসা পরিদর্শনের জন্য ভিসা এবং পাসপোর্ট",
+
+        'Visa-Descreiption'=>"InstaDoctor-এ আমাদের দল আপনাকে দ্রুত মেডিকেল ভিসা পেতে আপনার ভিসার আবেদন প্রস্তুত করতে সাহায্য করে। এছাড়াও আমরা চিকিৎসার উদ্দেশ্যে পাসপোর্ট এবং আন্তঃসীমান্ত ভ্রমণে সহায়তা করি। আপনার চিকিৎসা পরিদর্শনে সহায়তা পেতে নীচের নম্বরে আমাদের কল করুন।",
+
+        'Travel'=>"চিকিৎসা উদ্দেশ্যে ভ্রমণ সমর্থন",
+
+        'Travel-description'=>"আমরা বাস, ট্রেন এবং ফ্লাইটে সমস্ত ধরণের ভ্রমণ সহায়তা প্রদান করি। আপনার থাকার এবং খাওয়ার বিষয়ে চিন্তা করবেন না কারণ আমরা আপনার চিকিৎসা পরিদর্শনের সময় এটি পরিচালনা করি। প্রয়োজনে আমরা আপনাকে 24*7 সহায়তা করার জন্য একজন মেডিক্যাল অ্যাটেনডারের সাথে ট্যাগ করি।",
+
+        'Request For Appointment' => "নিয়োগের জন্য অনুরোধ",
+
+        'consult' => "পরামর্শদাতা"
+        ];
+?>
---
+++ en/msg.php
@@ -0,0 +1,103 @@
+<?php
+    return [
+        "Home" => "Home",
+        "Services" => "Services",
+        "Contact" => "Contact",
+        "Gallery" => "Gallery",
+        "Login" => "Login",
+        "Get the best" => "Get the best",
+        "Medical Consultation, Medicine Order, Medical Visit" => "Medical Consultation, Medicine Order, Medical Visit",
+        "and many more..." => "and many more...",
+        "\"Nature Image #2Nature Image #3" => "\"Nature Image #2Nature Image #3",
+        "Serving Patients" => "Serving Patients",
+        "in India, Nepal and Bangladesh" => "in India, Nepal and Bangladesh",
+        "Trusted VISA and Medical Visit Services" => "Trusted VISA and Medical Visit Services",
+
+        "Book Now" => "Book Now",
+        "Medicines Order and" => "Medicines Order and",
+        "Fastest Medicine delivery" => "Fastest Medicine delivery",
+        "and many more..." => "and many more...",
+        "Order Now" => "Order Now",
+        "Patient served" => "Patient served",
+        "Doctors tied" => "Doctors tied",
+        "Hospitals tied" => "Hospitals tied",
+
+        "Medical visits for patients" => "Medical visits for patients",
+        "Our Services" => "Our Services",
+        "Medical Consultation" => "Medical Consultation",
+        "Medicine order" => "Medicine order",
+        "lab test" => "lab test",
+        "Visa & Passports for medical visits:" => "Visa & Passports for medical visits:",
+        "Travel Support for medical purposes:" => "Travel Support for medical purposes:",
+        "Meet Our Experts" => "Meet Our Experts",
+        
+        "In today's society common people are not aware about their health-related information. Even though the current government has given them various health related facilities, the common people cannot get all those facilities properly, because they do not know the details about how and where to get those facilities. InstaDoctor PVT. LTD." => "In today's society common people are not aware about their health-related information. Even though the current government has given them various health related facilities, the common people cannot get all those facilities properly, because they do not know the details about how and where to get those facilities. InstaDoctor PVT. LTD.",
+        "Learn More" => "Learn More",
+        "A day in our lives" => "A day in our lives",
+        "Our Special Events" => "Our Special Events",
+        "Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics" => "Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics",
+        "Learn More" => "Learn More",
+        "Check out more Events" => "Check out more Events",
+        "We at work" => "We at work",
+        "Our Videos" => "Our Videos",
+        "Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics" => "Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics",
+        "Testimonials" => "Testimonials",
+
+        "Our Happy Customers" => "Our Happy Customers",
+        "Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics" => "Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics",
+        "Any Queries" => "Any Queries",
+        "Contact Us" => "Contact Us",
+        "Do you have any queries, suggestions, or requests? InstaDoctor is a chain of patient care facilities. To contact us, please fill out the form below." => "Do you have any queries, suggestions, or requests? InstaDoctor is a chain of patient care facilities. To contact us, please fill out the form below.",
+        "Contact Information" => "Contact Information",
+        "Your Name" => "Your Name",
+        "Your Email" => "Your Email",
+        "Your Email" => "Your Email",
+        "Head Office Address:" => "Head Office Address:",
+        "Hospital Road, Janaushadhi Building, 1st Floor, P.S : Madarihat, P.O : Madarihat, Dist: -Alipurduar, PIN: 735220" => "Hospital Road, Janaushadhi Building, 1st Floor, P.S : Madarihat, P.O : Madarihat, Dist: -Alipurduar, PIN: 735220",
+        "Phone Number:" => "Phone Number:",
+        "Whatsapp:" => "Whatsapp:",
+        "Email:" => "Email:",
+        "send" => "send",
+        "Menu" => "Menu",
+
+        "Doctors with us" => "Doctors with us",
+        "Hospitals with us" => "Hospitals with us",
+        "Terms and Conditions" => "Terms and Conditions",
+        "Return and Refund Policy" => "Return and Refund Policy",
+        "Shipping Policy" => "Shipping Policy",
+        "Privacy policy" => "Privacy policy",
+        "Download our Android App" => "Download our Android App",
+        "Get In Touch" => "Get In Touch",
+        "Kolkata Office: Prince Anwar shah road, Tollygaunge, Kolkata" => "Kolkata Office: Prince Anwar shah road, Tollygaunge, Kolkata",
+        "Nepal Office: Near Purono Bus Park, Bagbazar, Katmandu, 44600, Nepal" => "Nepal Office: Near Purono Bus Park, Bagbazar, Katmandu, 44600, Nepal",
+
+        'Contact' =>"Contact No",
+        'whatsapp'=>"Whatsapp",
+        'Email'=>"Email ",
+        'bannerTitle1'=>"Get Best Medical Consultations Medicine Order And Help On Your Medical Visits...",
+        'About' => "About Us",
+        'AboutDescription'=>"Our services help patients where distance is a critical factor. We bridge the gap and bring them in touch with the best medical facilities. Hence our services can help patients coming from distant places with facilities such as providing them with information for improved diagnosis and support for allopathic, homeopathic, and ayurvedic treatment. Our specific facilities will enable patients to acquire both online and offline consultations with our expert doctors, order medicine, request diagnostic tests, and get support with visa or passport-related matters, all ticketing inside India, pick up and drop services from the airport, train station and bus stop accommodation, medical guide and many more.",
+        'Services'=>"Our Services",
+        'Medical-Consultation'=>"Medical Consultation",
+
+        'MedicalDescription'=>"Are you looking to consult with trustworthy doctors and get medical facilities at an affordable cost? We have the best doctors registered with us who not only provide consultation online but also can be visited offline. Call us at the below number for further details.",
+        
+        'Medical-order'=>"Medicine Order",
+
+        'medicalOrder-description'=>"Do you want to order some medicine and wish them to be delivered in no time at a reasonable cost from outside India? We have a wonderful chain of delivery that ensures that your medicines reach you on time. Call us at the below number to order medicine.",
+
+        'labTest'=>"Lab Test",
+
+        'labTest-description'=>"Book your lab test with our InstaDoctor registered labs. Get all your Lab tests done at a reasonable rate and get hassle-free sample collection and report delivery. Call us at the below number to book a test.",
+
+        'Visa'=>"Visa & Passports for medical visits",
+
+        'Visa-Descreiption'=>"Our team in InstaDoctor helps you to prepare your visa application to get a quick medical visa. We also help with passport and cross-border travel for medical purposes. Call us at the below number to get assistance on your medical visits.",
+
+        'Travel'=>"Travel Support for medical purposes",
+
+        'Travel-description'=>"We provide all sorts of travel assistance by bus, train, and flight. Don't bother about your lodging and fooding as we manage that as well during your medical visits. If required we tag along a medical attender to assist you 24*7.",
+
+
+    ];
+?>
